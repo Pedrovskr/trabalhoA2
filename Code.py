ed198f0bae07bf98764229ae2423d6b7c4ea2bdd
Pedrovskr <133998796+Pedrovskr@users.noreply.github.com>
2023-06-06 08:37:19 -0300

    Update Code.py

--- Code.py
+++ Code.py
@@ -27,7 +27,11 @@ with st.expander('Lista de deputados'):
 
 
 st.header('Gráficos')
-st.header('Lista de deputados por estado')
+st.subheader('Número de deputados por partido')
+st.bar_chart(df['siglaPartido'].value_counts())
+st.subheader('Número de deputados por estado')
+st.bar_chart(df['siglaUf'].value_counts())
+
 col1, col2 = st.columns(2)
 state = col1.selectbox('Escolha um estado', sorted(df['siglaUf'].unique()), index=25)
 party = col2.selectbox('Escolha um partido', sorted(df['siglaPartido'].unique()))
