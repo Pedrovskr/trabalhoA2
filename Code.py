58e69c38ca2e32aafb82634f599c54607ae72776
Pedrovskr <133998796+Pedrovskr@users.noreply.github.com>
2023-06-06 08:15:16 -0300

    Update Code.py

--- Code.py
+++ Code.py
@@ -2,26 +2,24 @@ import streamlit as st
 import pandas as pd
 import requests
 
-def baixaDeputados(idLegislatura):
-    url = 'https://dadosabertos.camara.leg.br/api/v2/deputados?idLegislatura=' + str(idLegislatura)
-    r = requests.get(url)
-    deputados = r.json()['dados']
-    df = pd.DataFrame(deputados)
+def get_deputies(legislature_id):
+    url = f'https://dadosabertos.camara.leg.br/api/v2/deputados?idLegislatura={legislature_id}'
+    response = requests.get(url)
+    deputies = response.json()['dados']
+    df = pd.DataFrame(deputies)
     return df
 
-def obterDespesasDeputado(idDeputado):
-    url = 'https://dadosabertos.camara.leg.br/api/v2/deputados/' + str(idDeputado) + '/despesas'
-    r = requests.get(url)
-    despesas = r.json()['dados']
-    df = pd.DataFrame(despesas)
+def get_deputy_expenses(deputy_id):
+    url = f'https://dadosabertos.camara.leg.br/api/v2/deputados/{deputy_id}/despesas'
+    response = requests.get(url)
+    expenses = response.json()['dados']
+    df = pd.DataFrame(expenses)
     return df
 
 st.title('Lista de Deputados em Exercício')
 
-idLegislatura = st.slider('Escolha de qual legislatura você quer a lista de deputados', 50, 57, 57)
-
-df = baixaDeputados(idLegislatura)
-
+legislature_id = st.slider('Escolha de qual legislatura você quer a lista de deputados', 50, 57, 57)
+df = get_deputies(legislature_id)
 
 st.header('Lista de deputados')
 st.write(df)
@@ -34,28 +32,28 @@ st.subheader('Número de deputados por estado')
 st.bar_chart(df['siglaUf'].value_counts())
 
 st.header('Lista de deputados por estado')
-coluna1, coluna2 = st.columns(2)
-estado = coluna1.selectbox('Escolha um estado', sorted(df['siglaUf'].unique()), index=25)
-partido = coluna2.selectbox('Escolha um partido', sorted(df['siglaPartido'].unique()))
-df2 = df[(df['siglaUf'] == estado) & (df['siglaPartido'] == partido)]
+col1, col2 = st.columns(2)
+state = col1.selectbox('Escolha um estado', sorted(df['siglaUf'].unique()), index=25)
+party = col2.selectbox('Escolha um partido', sorted(df['siglaPartido'].unique()))
+df_filtered = df[(df['siglaUf'] == state) & (df['siglaPartido'] == party)]
 st.markdown('---')
 
-if df2.empty:
+if df_filtered.empty:
     st.subheader(':no_entry_sign: Sem deputados nesse estado filiados a esse partido! :crying_cat_face:')
 else:
-    total_despesas_partido = 0
-    for index, linha in df2.iterrows():
-        with st.expander(linha['nome']):
-            st.image(linha['urlFoto'], width=130)
-            st.write('Nome: ' + linha['nome'])
-            st.write('Partido: ' + linha['siglaPartido'])
-            st.write('UF: ' + linha['siglaUf'])
-            st.write('ID: ' + str(linha['id']))
-            st.write('Email: ' + str(linha['email']))
+    total_party_expenses = 0
+    for _, row in df_filtered.iterrows():
+        with st.expander(row['nome']):
+            st.image(row['urlFoto'], width=130)
+            st.write('Nome:', row['nome'])
+            st.write('Partido:', row['siglaPartido'])
+            st.write('UF:', row['siglaUf'])
+            st.write('ID:', row['id'])
+            st.write('Email:', row['email'])
             st.write('---')
             st.write('Despesas:')
-            despesas_df = obterDespesasDeputado(linha['id'])
-            st.write(despesas_df)
-            total_despesas_deputado = despesas_df['valorLiquido'].sum()
-            st.markdown(f'<h2 style="color:red;">Total de Despesas do Deputado: R${total_despesas_deputado:.2f}</h2>', unsafe_allow_html=True)
-            total_despesas_partido += total_despesas_deputado
+            expenses_df = get_deputy_expenses(row['id'])
+            st.write(expenses_df)
+            total_deputy_expenses = expenses_df['valorLiquido'].sum()
+            st.markdown(f'<h2 style="color:red;">Total de Despesas do Deputado: R${total_deputy_expenses:.2f}</h2>', unsafe_allow_html=True)
+            total_party_expenses += total_deputy_expenses
